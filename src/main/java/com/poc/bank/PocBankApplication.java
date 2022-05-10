fba11feb3af78e886d22ff57de288f283b781a04
dpuyol <45760837+dpuyol@users.noreply.github.com>
2022-05-10 17:55:34 +0200

    Primer commit spring initializer

---
+++ src/main/java/com/poc/bank/PocBankApplication.java
@@ -0,0 +1,13 @@
+package com.poc.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/bank/PocBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
