2bc54b50381818598c51d23f8476498b8857e3b4
DPuyol <davidpuyolruiz@gmail.com>
2022-05-18 21:11:17 +0200

    Add a findbyNumber to find by iban in a PortAccount

--- src/main/java/com/poc/bank/domain/dto/AccountDTO.java
+++ src/main/java/com/poc/bank/domain/dto/AccountDTO.java
@@ -5,10 +5,12 @@ import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
 @NoArgsConstructor
+@ToString
 public class AccountDTO {
 
 	private UUID id;
--- src/main/java/com/poc/bank/domain/incoming/InPortAccount.java
+++ src/main/java/com/poc/bank/domain/incoming/InPortAccount.java
@@ -1,11 +1,16 @@
 package com.poc.bank.domain.incoming;
 
+import java.util.Optional;
+
+import com.poc.bank.domain.dao.Account;
 import com.poc.bank.domain.dto.AccountDTO;
 
 public interface InPortAccount {
 
 	AccountDTO create(AccountDTO account);
 
-	AccountDTO addDepositAccount(AccountDTO account);
+	AccountDTO addDepositIbanAccount(AccountDTO account);
+
+	Optional<Account> findAccountByNumber(String number);
 
 }
